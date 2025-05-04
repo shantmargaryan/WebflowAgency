d5c72d0727b4ac6e5fd9132b04086a6ac34d9ddf
Shant <shant212205@gmail.com>
2025-05-03 23:03:22 -0800

    update

--- i18n/routing.js
+++ i18n/routing.js
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["am", "en"],
+  locales: ["en", "am"],
   // Used when no locale matches
   defaultLocale: "am",
 });
