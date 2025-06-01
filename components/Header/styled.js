7df41b6801b638d976c63c1d37dbb56b9d96e390
Shant <shant212205@gmail.com>
2025-05-31 22:36:23 -0800

    update

--- components/Header/styled.js
+++ components/Header/styled.js
@@ -73,7 +73,7 @@ export const NavList = styled.ul`
   @media (${({ theme }) => theme.media.laptop}) {
     flex-direction: row;
     justify-content: center;
-    padding: .5rem;
+    padding: .625rem .7rem;
     background-color: ${({ theme }) => theme.colors.yellow};
     border-radius: 2.5625rem;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
